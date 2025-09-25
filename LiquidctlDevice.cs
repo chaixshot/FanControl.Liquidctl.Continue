23b336fd9991fac7ad7427b2a848f80c8aa78ca1
chaixshot <chai5142xshot@gmail.com>
2025-09-25 20:10:58 +0700

    change: Get native Fan Duty percentage from Liquidctl

--- LiquidctlDevice.cs
+++ LiquidctlDevice.cs
@@ -75,22 +75,10 @@ namespace FanControl.Liquidctl
 
             public void UpdateFromJSON(LiquidctlStatusJSON output)
             {
-                float reading = (float)output.status.Single(entry => entry.key == KEY).value;
-                _value = reading > MAX_RPM ? 100.0f : (float)Math.Ceiling(100.0f * reading / MAX_RPM);
-                //_value = RPM_LOOKUP.OrderBy(e => Math.Abs(e.Key - reading)).FirstOrDefault().Value;
+                _value = (float)output.status.Single(entry => entry.key == KEY).value;
             }
 
-            public static readonly string KEY = "Pump speed";
-            static readonly Dictionary<int, int> RPM_LOOKUP = new Dictionary<int, int>
-            { // We can only estimate, as it is not provided in any output. Hence I applied this ugly hack
-                {1200, 40}, {1206, 41}, {1212, 42}, {1218, 43}, {1224, 44}, {1230, 45}, {1236, 46}, {1242, 47}, {1248, 48}, {1254, 49},
-                {1260, 50}, {1313, 51}, {1366, 52}, {1419, 53}, {1472, 54}, {1525, 55}, {1578, 56}, {1631, 57}, {1684, 58}, {1737, 59},
-                {1790, 60}, {1841, 61}, {1892, 62}, {1943, 63}, {1994, 64}, {2045, 65}, {2096, 66}, {2147, 67}, {2198, 68}, {2249, 69},
-                {2300, 70}, {2330, 71}, {2360, 72}, {2390, 73}, {2420, 74}, {2450, 75}, {2480, 76}, {2510, 77}, {2540, 78}, {2570, 79},
-                {2600, 80}, {2618, 81}, {2636, 82}, {2654, 83}, {2672, 84}, {2690, 85}, {2708, 86}, {2726, 87}, {2744, 88}, {2762, 89},
-                {2780, 90}, {2789, 91}, {2798, 92}, {2807, 93}, {2816, 94}, {2825, 95}, {2834, 96}, {2843, 97}, {2852, 98}, {2861, 99},
-                {MAX_RPM, 100}
-            };
+            public static readonly string KEY = "Pump duty";
 
             static readonly int MAX_RPM = 2800;
 
@@ -147,9 +135,9 @@ namespace FanControl.Liquidctl
             { } // plugin updates sensors
         }
         
-        public class FanControl : IPluginControlSensor
+        public class FanDuty : IPluginControlSensor
         {
-            public FanControl(LiquidctlStatusJSON output)
+            public FanDuty(LiquidctlStatusJSON output)
             {
                 _address = output.address;
                 _id = $"{output.address}-fanctrl";
@@ -160,24 +148,10 @@ namespace FanControl.Liquidctl
             // We can only estimate, as it is not provided in any output
             public void UpdateFromJSON(LiquidctlStatusJSON output)
             {
-                float reading = (float)output.status.Single(entry => entry.key == KEY).value;
-                _value = reading > MAX_RPM ? 100.0f : (float)Math.Ceiling(100.0f * reading / MAX_RPM);
-                //_value = RPM_LOOKUP.OrderBy(e => Math.Abs(e.Key - reading)).FirstOrDefault().Value;
+                _value = (float)output.status.Single(entry => entry.key == KEY).value;
             }
 
-            public static readonly string KEY = "Fan speed";
-            static readonly Dictionary<int, int> RPM_LOOKUP = new Dictionary<int, int>
-            { // We can only estimate, as it is not provided in any output. Hence I applied this ugly hack
-                {520, 20}, {521, 21}, {522, 22}, {523, 23}, {524, 24}, {525, 25}, {526, 26}, {527, 27}, {528, 28}, {529, 29},
-                {530, 30}, {532, 31}, {534, 32}, {536, 33}, {538, 34}, {540, 35}, {542, 36}, {544, 37}, {546, 38}, {548, 39},
-                {550, 40}, {571, 41}, {592, 42}, {613, 43}, {634, 44}, {655, 45}, {676, 46}, {697, 47}, {718, 48}, {739, 49},
-                {760, 50}, {781, 51}, {802, 52}, {823, 53}, {844, 54}, {865, 55}, {886, 56}, {907, 57}, {928, 58}, {949, 59},
-                {970, 60}, {989, 61}, {1008, 62}, {1027, 63}, {1046, 64}, {1065, 65}, {1084, 66}, {1103, 67}, {1122, 68}, {1141, 69},
-                {1160, 70}, {1180, 71}, {1200, 72}, {1220, 73}, {1240, 74}, {1260, 75}, {1280, 76}, {1300, 77}, {1320, 78}, {1340, 79},
-                {1360, 80}, {1377, 81}, {1394, 82}, {1411, 83}, {1428, 84}, {1445, 85}, {1462, 86}, {1479, 87}, {1496, 88}, {1513, 89},
-                {1530, 90}, {1550, 91}, {1570, 92}, {1590, 93}, {1610, 94}, {1630, 95}, {1650, 96}, {1670, 97}, {1690, 98}, {1720, 99},
-                {MAX_RPM, 100}
-            };
+            public static readonly string KEY = "Fan duty";
 
             static readonly int MAX_RPM = 1980;
 
@@ -233,9 +207,9 @@ namespace FanControl.Liquidctl
             { } // plugin updates sensors
         }
 
-        public class MicroFanControl : IPluginControlSensor
+        public class MicroFanDuty : IPluginControlSensor
         {
-            public MicroFanControl(LiquidctlStatusJSON output)
+            public MicroFanDuty(LiquidctlStatusJSON output)
             {
                 _address = output.address;
                 _id = $"{output.address}-fanctrl";
@@ -246,24 +220,10 @@ namespace FanControl.Liquidctl
             // We can only estimate, as it is not provided in any output
             public void UpdateFromJSON(LiquidctlStatusJSON output)
             {
-                float reading = (float)output.status.Single(entry => entry.key == KEY).value;
-                _value = reading > MAX_RPM ? 100.0f : (float)Math.Ceiling(100.0f * reading / MAX_RPM);
-                //_value = RPM_LOOKUP.OrderBy(e => Math.Abs(e.Key - reading)).FirstOrDefault().Value;
+                _value = (float)output.status.Single(entry => entry.key == KEY).value;
             }
 
-            public static readonly string KEY = "Pump fan speed";
-            static readonly Dictionary<int, int> RPM_LOOKUP = new Dictionary<int, int>
-            { // We can only estimate, as it is not provided in any output. Hence I applied this ugly hack
-                {520, 20}, {521, 21}, {522, 22}, {523, 23}, {524, 24}, {525, 25}, {526, 26}, {527, 27}, {528, 28}, {529, 29},
-                {530, 30}, {532, 31}, {534, 32}, {536, 33}, {538, 34}, {540, 35}, {542, 36}, {544, 37}, {546, 38}, {548, 39},
-                {550, 40}, {571, 41}, {592, 42}, {613, 43}, {634, 44}, {655, 45}, {676, 46}, {697, 47}, {718, 48}, {739, 49},
-                {760, 50}, {781, 51}, {802, 52}, {823, 53}, {844, 54}, {865, 55}, {886, 56}, {907, 57}, {928, 58}, {949, 59},
-                {970, 60}, {989, 61}, {1008, 62}, {1027, 63}, {1046, 64}, {1065, 65}, {1084, 66}, {1103, 67}, {1122, 68}, {1141, 69},
-                {1160, 70}, {1180, 71}, {1200, 72}, {1220, 73}, {1240, 74}, {1260, 75}, {1280, 76}, {1300, 77}, {1320, 78}, {1340, 79},
-                {1360, 80}, {1377, 81}, {1394, 82}, {1411, 83}, {1428, 84}, {1445, 85}, {1462, 86}, {1479, 87}, {1496, 88}, {1513, 89},
-                {1530, 90}, {1550, 91}, {1570, 92}, {1590, 93}, {1610, 94}, {1630, 95}, {1650, 96}, {1670, 97}, {1690, 98}, {1720, 99},
-                {MAX_RPM, 100}
-            };
+            public static readonly string KEY = "Pump fan duty";
 
             static readonly int MAX_RPM = 5200;
 
@@ -322,9 +282,9 @@ namespace FanControl.Liquidctl
         }
 
         // Try to control multiple fans
-        public class FanControlMultiple : IPluginControlSensor
+        public class FanDutyMultiple : IPluginControlSensor
         {
-            public FanControlMultiple(int index, LiquidctlStatusJSON output)
+            public FanDutyMultiple(int index, LiquidctlStatusJSON output)
             {
                 _address = output.address;
                 _id = $"{output.address}-fan{index}ctrl";
@@ -337,26 +297,11 @@ namespace FanControl.Liquidctl
             // We can only estimate, as it is not provided in any output
             public void UpdateFromJSON(int index, LiquidctlStatusJSON output) {
                 string currentKey = FanSpeedMultiple.KEY.Replace("###", index.ToString());
-                float reading = (float)output.status.Single(entry => entry.key == currentKey).value;
-                //_value = reading > MAX_RPM ? 100.0f : (float)Math.Ceiling(100.0f * reading / MAX_RPM);
-                _value = RPM_LOOKUP.OrderBy(e => Math.Abs(e.Key - reading)).FirstOrDefault().Value;
+                _value = (float)output.status.Single(entry => entry.key == currentKey).value;
             }
 
-            public static string KEY = "Fan ### speed";
+            public static string KEY = "Fan ### dufy";
             //public static string KEY = $"Fan {_index} speed";
-
-            static readonly Dictionary<int, int> RPM_LOOKUP = new Dictionary<int, int>
-            { // We can only estimate, as it is not provided in any output. Hence I applied this ugly hack
-                {520, 20}, {521, 21}, {522, 22}, {523, 23}, {524, 24}, {525, 25}, {526, 26}, {527, 27}, {528, 28}, {529, 29},
-                {530, 30}, {532, 31}, {534, 32}, {536, 33}, {538, 34}, {540, 35}, {542, 36}, {544, 37}, {546, 38}, {548, 39},
-                {550, 40}, {571, 41}, {592, 42}, {613, 43}, {634, 44}, {655, 45}, {676, 46}, {697, 47}, {718, 48}, {739, 49},
-                {760, 50}, {781, 51}, {802, 52}, {823, 53}, {844, 54}, {865, 55}, {886, 56}, {907, 57}, {928, 58}, {949, 59},
-                {970, 60}, {989, 61}, {1008, 62}, {1027, 63}, {1046, 64}, {1065, 65}, {1084, 66}, {1103, 67}, {1122, 68}, {1141, 69},
-                {1160, 70}, {1180, 71}, {1200, 72}, {1220, 73}, {1240, 74}, {1260, 75}, {1280, 76}, {1300, 77}, {1320, 78}, {1340, 79},
-                {1360, 80}, {1377, 81}, {1394, 82}, {1411, 83}, {1428, 84}, {1445, 85}, {1462, 86}, {1479, 87}, {1496, 88}, {1513, 89},
-                {1530, 90}, {1550, 91}, {1570, 92}, {1590, 93}, {1610, 94}, {1630, 95}, {1650, 96}, {1670, 97}, {1690, 98}, {1720, 99},
-                {MAX_RPM, 100}
-            };
 
             static readonly int MAX_RPM = 1980;
 
@@ -404,13 +349,13 @@ namespace FanControl.Liquidctl
             hasFanSpeed = output.status.Exists(entry => entry.key == FanSpeed.KEY && !(entry.value is null));
             if (hasFanSpeed) {
                 fanSpeed = new FanSpeed(output);
-                fanControl = new FanControl(output);
+                fanControl = new FanDuty(output);
             }
 
             hasMicroFanSpeed = output.status.Exists(entry => entry.key == MicroFanSpeed.KEY && !(entry.value is null));
             if (hasMicroFanSpeed) {
                 microFanSpeed = new MicroFanSpeed(output);
-                microFanControl = new MicroFanControl(output);
+                microFanControl = new MicroFanDuty(output);
             }
 
             hasLiquidTemperature = output.status.Exists(entry => entry.key == LiquidTemperature.KEY && !(entry.value is null));
@@ -427,7 +372,7 @@ namespace FanControl.Liquidctl
 
                 if (hasMultipleFanSpeed[i]) {
                     fanSpeedMultiple[i] = new FanSpeedMultiple(index, output);
-                    fanControlMultiple[i] = new FanControlMultiple(index, output);
+                    fanControlMultiple[i] = new FanDutyMultiple(index, output);
                 }
             }
         }
@@ -466,12 +411,12 @@ namespace FanControl.Liquidctl
         public PumpSpeed pumpSpeed;
         public PumpDuty pumpDuty;
         public FanSpeed fanSpeed;
-        public FanControl fanControl;
+        public FanDuty fanControl;
         public MicroFanSpeed microFanSpeed;
-        public MicroFanControl microFanControl;
+        public MicroFanDuty microFanControl;
 
         public FanSpeedMultiple[] fanSpeedMultiple = new FanSpeedMultiple[20];
-        public FanControlMultiple[] fanControlMultiple = new FanControlMultiple[20];
+        public FanDutyMultiple[] fanControlMultiple = new FanDutyMultiple[20];
 
 
         public void LoadJSON()
